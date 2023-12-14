ba2a91e05119b6726069feb2071d9f36fb7dae4c
alanlasco <61526254+alanlasco@users.noreply.github.com>
2023-12-14 17:16:35 -0300

    adding tests first try

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaMagnifyingGlass } from "react-icons/fa6";
 
 
 
@@ -8,9 +9,13 @@ export const Home = () => {
   return (
     <>
    
-    <div className='flex flex-col justify-center items-center h-screen bg-zinc-900 container mx-auto'>
+    <div data-testid="home-component" className='flex flex-col justify-center items-center h-screen bg-zinc-900 w-full'>
       <img className='w-1/2 max-w-lg h-auto md:w-1/2 sm:w-2/3' src={require(`../images/rickandmorty.png`)} alt="" />
-        <button className='bg-green-400 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full'><Link to="./browser" >Browse</Link></button> 
+        <button className='bg-green-400 hover:bg-green-700 text-white font-bold py-2 px-6 rounded-full'>  <div className="flex items-center">
+          <Link to="./browser" className="flex items-center">
+            Explore<FaMagnifyingGlass className="ml-2" />
+          </Link>
+        </div></button> 
     </div>
     </>
   );
