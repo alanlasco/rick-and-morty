d0b056fdaf1e4ff9a09dc39cc5a794baf6122209
alanlasco <61526254+alanlasco@users.noreply.github.com>
2023-12-13 21:43:43 -0300

    adding dependences, home image

---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div className='flex justify-center items-center h-screen bg-zinc-900'>
+    
+      <img className='w-1/2 max-w-lg h-auto md:w-1/2 sm:w-1/2' src={require(`../images/rickandmorty.png`)} alt="" />
+
+    </div>
+  )
+}
