9d06faee54a21aed4308f10612b11674e1b13088
alanlasco <61526254+alanlasco@users.noreply.github.com>
2023-12-14 16:22:28 -0300

    adding btn browse

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Home } from './components/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Browser } from './components/Browser';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/browser" element={<Browser />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
 
 export const Home = () => {
   return (
-    <div className='flex justify-center items-center h-screen bg-zinc-900'>
-    
-      <img className='w-1/2 max-w-lg h-auto md:w-1/2 sm:w-1/2' src={require(`../images/rickandmorty.png`)} alt="" />
-
+    <>
+   
+    <div className='flex flex-col justify-center items-center h-screen bg-zinc-900 container mx-auto'>
+      <img className='w-1/2 max-w-lg h-auto md:w-1/2 sm:w-2/3' src={require(`../images/rickandmorty.png`)} alt="" />
+        <button className='bg-green-400 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full'><Link to="./browser" >Browse</Link></button> 
     </div>
-  )
-}
+    </>
+  );
+};
