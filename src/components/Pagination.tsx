5c86c1f37a761b87bc389688da5ef2e256038704
alanlasco <61526254+alanlasco@users.noreply.github.com>
2023-12-16 19:19:05 -0300

    adding buttons style

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -5,9 +5,35 @@ export const Pagination = (props: PaginationProps) => {
   return (
     <div className="flex container">
       {props.previous ? (
-        <button onClick={() => props.onPrevious()}>Previous</button>
-      ) : null}
-      {props.next ? <button onClick={() => props.onNext()}>Next</button> : null}
+        <button
+          onClick={() => props.onPrevious()}
+          className="bg-green-400 hover:bg-green-700 text-white font-bold py-2 px-6 rounded-full"
+        >
+          <div className="flex items-center">Previous</div>
+        </button>
+      ) : (
+        <button
+          disabled
+          className="disabled:pointer-events-none bg-green-100  text-white font-bold py-2 px-6 rounded-full"
+        >
+          <div className="flex items-center">Previous</div>
+        </button>
+      )}
+      {props.next ? (
+        <button
+          onClick={() => props.onNext()}
+          className="bg-green-400 hover:bg-green-700 text-white font-bold py-2 px-6 rounded-full"
+        >
+          <div className="flex items-center">Next</div>
+        </button>
+      ) : (
+        <button
+          disabled
+          className="disabled:pointer-events-none bg-green-100  text-white font-bold py-2 px-6 rounded-full"
+        >
+          <div className="flex items-center">Next</div>
+        </button>
+      )}
     </div>
   );
 };
